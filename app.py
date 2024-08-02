1a20e391b51a8552d12fd6c3ff5ea5e367c0d7ac
Eunji_Jun <eternitymoon@naver.com>
2024-08-03 02:58:41 +0900

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+# streamlit_app.py
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(
+    worksheet="WARNING_TEST",
+    ttl="10m",
+    usecols=[0, 1],
+)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
