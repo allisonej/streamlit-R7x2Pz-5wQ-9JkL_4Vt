317a5065f601beeb8fa0a5961b2fd7fbbfb64538
Eunji_Jun <eternitymoon@naver.com>
2024-08-12 14:44:05 +0900

    update try-except +graph+cache

--- pages/app copy.py
+++ pages/app copy.py
@@ -20,85 +20,92 @@ def load_key(url):
     key = pd.read_csv(StringIO(response.text))
     return key
 
+@st.cache_data  # process_files 함수에도 캐싱 적용
 def process_files(uploaded_file, answer_key):
-    # 업로드된 CSV 파일 읽기
-    user_df = pd.read_csv(uploaded_file)
+    try:
+        # 업로드된 CSV 파일 읽기
+        user_df = pd.read_csv(uploaded_file)
 
-    st.write("업로드 파일 ID 수: ", len(user_df), "\t정답 파일 ID 수: ", len(answer_key))
-    st.write("target na? : ", user_df['target'].isna().sum())
-    st.markdown("---")
-    
-    # 데이터 처리
-    merged_df = pd.merge(user_df, answer_key, on='ID')
-    changed_df = merged_df[merged_df['target'] != merged_df['label']]
-    
-    # Macro F1 Score 계산
-    macro_f1 = f1_score(merged_df['label'], merged_df['target'], average='macro')
-    st.markdown(f"Macro F1 Score: **:blue[{macro_f1:.4f}]**")
-
-    # 분석 결과 출력
-    if not changed_df.empty:
-        st.write("정답이 틀린 항목에 대한 분석표입니다.")
-        st.write(f"총 틀린 항목 수: ", len(changed_df))
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.write("ID, 예측값, 정답:")
-            st.dataframe(changed_df[['ID', 'target', 'label']])
-            # 결과를 CSV로 저장
-            csv_data = changed_df.to_csv(index=False)
-            st.download_button(
-                label="Download Result CSV",
-                data=csv_data,
-                file_name='compare_asr.csv'
-            )
-        
-        with col2:
-            st.write("[예측값, 정답] 조합의 빈도수:")
-            pair_counts = changed_df.groupby(['target', 'label']).size().reset_index(name='count')
-            pair_counts_sorted = pair_counts.sort_values(by='count', ascending=False)
-            st.write(pair_counts_sorted)
-            # 결과를 CSV로 저장
-            csv_data = pair_counts_sorted.to_csv(index=False)
-            st.download_button(
-                label="Download Pair CSV",
-                data=csv_data,
-                file_name='pair.csv'
-            )
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("틀린 예측값 빈도, 틀린 비율: target으로 잘못 예측한 수 / target으로 예측한 전체 수")
-            wrong_counts = changed_df['target'].value_counts() # 예측값 틀린 것 셈
-            total_counts = merged_df['target'].value_counts() # 예측값 전체 셈
-            counts_combined = pd.concat([wrong_counts, total_counts], axis=1, sort=False).fillna(0)
-            counts_combined.columns = ['wrong_count', 'total_count']
-            counts_combined['rate'] = counts_combined['wrong_count'] / counts_combined['total_count']
-            counts_combined['rate_view'] = counts_combined.apply(lambda row: f"{int(row['wrong_count'])} / {int(row['total_count'])}", axis=1)
-            st.write(counts_combined[['wrong_count', 'rate_view', 'rate']])
-        
-        with col2:
-            st.write("못 맞춘 정답 빈도, 틀린 비율: 잘못 예측한 실제값 수 / 전체 실제값 수")
-            wrong_counts = changed_df['label'].value_counts() # 실제값 틀린 것 셈
-            total_counts = merged_df['label'].value_counts() # 실제값 전체 셈
-            counts_combined = pd.concat([wrong_counts, total_counts], axis=1, sort=False).fillna(0)
-            counts_combined.columns = ['wrong_count', 'total_count']
-            counts_combined['rate'] = counts_combined['wrong_count'] / counts_combined['total_count']
-            counts_combined['rate_view'] = counts_combined.apply(lambda row: f"{int(row['wrong_count'])} / {int(row['total_count'])}", axis=1)
-            st.write(counts_combined[['wrong_count', 'rate_view', 'rate']])
-
-        # 추가: 혼동 행렬(Confusion Matrix) 시각화
+        st.write("업로드 파일 ID 수: ", len(user_df), "\t정답 파일 ID 수: ", len(answer_key))
+        st.write("target na? : ", user_df['target'].isna().sum())
         st.markdown("---")
-        st.subheader("Confusion Matrix")
-        cm = confusion_matrix(merged_df['label'], merged_df['target'])
-        fig, ax = plt.subplots()
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=ax)
-        ax.set_xlabel('Predicted')
-        ax.set_ylabel('True')
-        st.pyplot(fig)
 
-    else:
-        st.write("변경된 target 값이 없습니다.")
+        # 데이터 처리 (join 사용)
+        merged_df = user_df.set_index('ID').join(answer_key.set_index('ID'))
+        changed_df = merged_df[merged_df['target'] != merged_df['label']]
+
+        # Macro F1 Score 계산
+        macro_f1 = f1_score(merged_df['label'], merged_df['target'], average='macro')
+        st.markdown(f"Macro F1 Score: **:blue[{macro_f1:.4f}]**")
+
+        # 분석 결과 출력
+        if not changed_df.empty:
+            st.write("정답이 틀린 항목에 대한 분석표입니다.")
+            st.write(f"총 틀린 항목 수: ", len(changed_df))
+            col1, col2 = st.columns(2)
+
+            with col1:
+                st.write("ID, 예측값, 정답:")
+                st.dataframe(changed_df.reset_index()[['ID', 'target', 'label']])  # 인덱스 초기화
+                # 결과를 CSV로 저장
+                csv_data = changed_df.reset_index().to_csv(index=False) 
+                st.download_button(
+                    label="Download Result CSV",
+                    data=csv_data,
+                    file_name='compare_asr.csv'
+                )
+
+            with col2:
+                st.write("[예측값, 정답] 조합의 빈도수:")
+                pair_counts = changed_df.groupby(['target', 'label']).size().reset_index(name='count')
+                pair_counts_sorted = pair_counts.sort_values(by='count', ascending=False)
+                st.write(pair_counts_sorted)
+                # 결과를 CSV로 저장
+                csv_data = pair_counts_sorted.to_csv(index=False)
+                st.download_button(
+                    label="Download Pair CSV",
+                    data=csv_data,
+                    file_name='pair.csv'
+                )
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write("틀린 예측값 빈도, 틀린 비율: target으로 잘못 예측한 수 / target으로 예측한 전체 수")
+                wrong_counts = changed_df['target'].value_counts() # 예측값 틀린 것 셈
+                total_counts = merged_df['target'].value_counts() # 예측값 전체 셈
+                counts_combined = pd.concat([wrong_counts, total_counts], axis=1, sort=False).fillna(0)
+                counts_combined.columns = ['wrong_count', 'total_count']
+                counts_combined['rate'] = counts_combined['wrong_count'] / counts_combined['total_count']
+                counts_combined['rate_view'] = counts_combined.apply(lambda row: f"{int(row['wrong_count'])} / {int(row['total_count'])}", axis=1)
+                st.write(counts_combined[['wrong_count', 'rate_view', 'rate']])
+
+            with col2:
+                st.write("못 맞춘 정답 빈도, 틀린 비율: 잘못 예측한 실제값 수 / 전체 실제값 수")
+                wrong_counts = changed_df['label'].value_counts() # 실제값 틀린 것 셈
+                total_counts = merged_df['label'].value_counts() # 실제값 전체 셈
+                counts_combined = pd.concat([wrong_counts, total_counts], axis=1, sort=False).fillna(0)
+                counts_combined.columns = ['wrong_count', 'total_count']
+                counts_combined['rate'] = counts_combined['wrong_count'] / counts_combined['total_count']
+                counts_combined['rate_view'] = counts_combined.apply(lambda row: f"{int(row['wrong_count'])} / {int(row['total_count'])}", axis=1)
+                st.write(counts_combined[['wrong_count', 'rate_view', 'rate']])
+
+            # 추가: 혼동 행렬(Confusion Matrix) 시각화 개선
+            st.markdown("---")
+            st.subheader("Confusion Matrix")
+            cm = confusion_matrix(merged_df['label'], merged_df['target'])
+            fig, ax = plt.subplots()
+            sns.heatmap(cm, annot=True, fmt='d', cmap='Oranges', ax=ax)  # 컬러맵 변경
+            ax.set_xlabel('Predicted')
+            ax.set_ylabel('True')
+            # 레이블 설정
+            ax.set_xticklabels(merged_df['target'].unique())
+            ax.set_yticklabels(merged_df['label'].unique())
+            st.pyplot(fig)
+
+        else:
+            st.write("변경된 target 값이 없습니다.")
+    except Exception as e:
+        st.error(f"오류가 발생했습니다: {e}")
 
 # 사이드바에 메시지 추가
 st.sidebar.write("우측 메뉴에서 wide mode를 적용해주세요.")
